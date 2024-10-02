da78493517d58b2c122171c7bfe8d79633589ac6
mtroesser <mtroesser@live.com>
2024-10-01 20:04:20 -0500

    Shrey: created a little auto routine in meep meep called ShreyAutoTest, not finished because I made a mistake with the .actionBuilder().

--- MeepMeep_0_1_6/src/main/java/Into_The_Deep/GeronimoAutoTest.java
+++ MeepMeep_0_1_6/src/main/java/Into_The_Deep/GeronimoAutoTest.java
@@ -1,11 +1,12 @@
 package Into_The_Deep;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 public class GeronimoAutoTest {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
        // static Pose2d startPose;
     // Testing from home
@@ -16,15 +17,19 @@ public class GeronimoAutoTest {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(48)
+        Pose2d StartPose = new Pose2d(-12, -60, 90);
+        Vector2d VectorTwo = new Vector2d(-12, -30);
+        myBot.runAction(myBot.getDrive().actionBuilder(StartPose)
+                           //  .splineToLinearHeading(new Pose2d(36,48,36.6), Math.toRadians(270))
+                                .strafeToLinearHeading(VectorTwo, Math.toRadians(90))
+                                 .strafeToLinearHeading(new Vector2d(-12, -36), Math.toRadians(225))
+                                .strafeToLinearHeading(new Vector2d(-48, -48), Math.toRadians(225))
+               // .turn(Math.toRadians(270))
+              /*  .lineToX(0)
                 .turn(Math.toRadians(90))
-                .lineToY(48)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+
+               */ //.turn(Math.toRadians(90))
+               //  .lineToY(-24)
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
