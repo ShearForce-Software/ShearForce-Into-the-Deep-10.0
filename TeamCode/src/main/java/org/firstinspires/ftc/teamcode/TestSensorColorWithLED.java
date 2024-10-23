556e57d4c650089ebff4b2aa8e763591d8522f4c
Matthew <matthewfunk@stdominichs.org>
2024-10-22 19:51:50 -0500

    Matthew : created 2 new test color sensor classes to test with and without the LED on next time with.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSensorColorWithLED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSensorColorWithLED.java
@@ -33,6 +33,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
