32aff4629a9e13f12503b6b32f950865a7302e27
Matthew <matthewfunk@stdominichs.org>
2024-10-29 18:45:42 -0500

    Matthew : added the necessary code to detect colors and add telemetry to the driver station!!!!!!!!!! :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSensorColorWithLED.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestSensorColorWithLED.java
@@ -194,6 +194,7 @@ public class TestSensorColorWithLED extends LinearOpMode {
       // Update the hsvValues array by passing it to Color.colorToHSV()
       Color.colorToHSV(colors.toColor(), hsvValues);
 
+      /* NoBox
       // Red HSV Color ranges
       double hMinRed = 17.903;
       double hMaxRed = 120.000;
@@ -217,20 +218,46 @@ public class TestSensorColorWithLED extends LinearOpMode {
       double sMaxBlue = 0.832;
       double vMinBlue = 0.071;
       double vMaxBlue = 1.000;
+      */
+
+      // Box
+      // Red HSV Color ranges
+      double hMinRed = 19.811;
+      double hMaxRed = 128.000;
+      double sMinRed = 0.397;
+      double sMaxRed = 0.671;
+      double vMinRed = 0.263;
+      double vMaxRed = 0.722;
+
+      // Yellow HSV Color Values
+      double hMinYellow = 59.000;
+      double hMaxYellow = 113.043;
+      double sMinYellow = 0.501;
+      double sMaxYellow = 0.702;
+      double vMinYellow = 0.565;
+      double vMaxYellow = 1.000;
+
+      // Blue HSV Color Values
+      double hMinBlue = 187.152;
+      double hMaxBlue = 219.568;
+      double sMinBlue = 0.741;
+      double sMaxBlue = 0.832;
+      double vMinBlue = 0.514;
+      double vMaxBlue = 1.000;
+
 
       // determine if color is blue, red or yellow and show telemetry
-      if (hsvValues[0] >= hMinBlue)
+      if (hsvValues[0] >= hMinBlue && hsvValues[1] >= sMinBlue)
         telemetry.addLine("Blue detected");
 
-      else if (hsvValues[2] <= vMinYellow)
-        if (hsvValues[1] >= sMaxYellow || hsvValues[1] <= sMinYellow) {
-          telemetry.addLine("Red detected 1");
-          if ((hsvValues[0] <= hMinYellow) || (hsvValues[0] >= hMaxYellow && hsvValues[0] <= hMinBlue))
-            telemetry.addLine("Red detected 2");
-        }
-
-      else /*if (hsvValues[1] <= sMaxYellow && hsvValues[1] >= sMinYellow)*/
+      else if (hsvValues[1] <= sMaxYellow && hsvValues[1] >= sMinYellow && hsvValues[0] <= hMaxYellow && hsvValues[0] >= hMinYellow)
         telemetry.addLine("Yellow detected");
+
+      else if (hsvValues[1] <= sMaxRed && hsvValues[1] >= sMinRed && hsvValues[0] <= hMaxRed && hsvValues[0] >= hMinRed)
+        telemetry.addLine("Red detected");
+
+      else
+        telemetry.addLine("No color detected");
 
       telemetry.addLine()
               .addData("Red", "%.3f", colors.red)
