b783ed52dce08334f31fe3c6bc62c89ec81dd189
xavier <xavierzimmerman22@gmail.com>
2024-11-05 20:06:12 -0600

    Updated To Do List. Added TestFourArmMotors_LimitSwitches to TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFourArmMotors_LimitSwitches.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestFourArmMotors_LimitSwitches.java
@@ -32,6 +32,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -64,8 +65,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Test FourArmMotors_LimitSwitches", group="Test")
-@Disabled
+@TeleOp(name="Test FourArmMotors_LimitSwitches", group="Test")
+//@Disabled
 public class TestFourArmMotors_LimitSwitches extends LinearOpMode {
 
     /* Declare OpMode members. */
