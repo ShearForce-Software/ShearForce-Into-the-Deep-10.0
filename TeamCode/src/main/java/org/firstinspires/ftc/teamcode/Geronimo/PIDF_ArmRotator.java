93011cfaa63ae27645675eab9accce10f3d874d8
Claire-25 <136668541+Claire-25@users.noreply.github.com>
2024-12-23 16:54:01 -0600

    PIDF changes to gradle

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Geronimo/PIDF_ArmRotator.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode.Geronimo;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Config
+@TeleOp
+public class PIDF_ArmRotator extends OpMode {
+    private PIDController controller;
+
+    public static double p = 0, i = 0, d = 0;
+    public static double f = 0;
+
+    public static int target = 0;
+
+    //TODO
+    private final double ticks_in_degree = 537.6/360.0; //I'm not sure if this is the correct number for our motors
+
+    private DcMotor leftSlideArmRotatorMotor;
+    private DcMotor rightSlideArmRotatorMotor;
+        @Override
+        public void init(){
+            controller = new PIDController(p, i, d);
+            telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+            leftSlideArmRotatorMotor = hardwareMap.get(DcMotorEx.class, "leftRotater");
+            rightSlideArmRotatorMotor = hardwareMap.get(DcMotorEx.class, "rightRotater");
+
+        }
+
+        @Override
+        public void loop(){
+            controller.setPID(p, i, d);
+
+            //Only basing it off the left rotator
+            int armPosLeft = leftSlideArmRotatorMotor.getCurrentPosition();
+            double pid = controller.calculate(armPosLeft, target);
+            double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
+
+            double power = pid + ff;
+
+            //setting power to both rotating motors
+            leftSlideArmRotatorMotor.setPower(power);
+            rightSlideArmRotatorMotor.setPower(power);
+
+            telemetry.addData("pos", armPosLeft);
+            telemetry.addData("target", target);
+            telemetry.update();
+
+        }
+
+
+}
+
